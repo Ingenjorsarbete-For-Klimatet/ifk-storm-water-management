6fafc91ffbe282191578a36a68faf6b046c9492b
Christoffer Strömberg <chris.stromberg@icloud.com>
2024-04-21 19:17:02 +0200

    first commit

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to ifk-storm-water-management
+
+Docs for ifk-storm-water-management.
---
+++ src/storm_water_management/storm_water_management.py
@@ -0,0 +1,6 @@
+"""storm_water_management module."""
+import rasterio
+
+def temp():
+    """Temporary function."""
+    return True
