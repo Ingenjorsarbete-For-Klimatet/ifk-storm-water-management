fa0396ad2bc138e45d13ef5cd720e92df23a0788
Christoffer Strömberg <chris.stromberg@icloud.com>
2024-09-28 21:56:32 +0200

    main added, cpu speed improved

--- src/storm_water_management/main.py
+++ src/storm_water_management/main.py
@@ -1,1 +1,41 @@
+'''From elevation data, utilize flow and depression analysis, and save as geojson
+'''
+import richdem as rd
+from storm_water_management import utils
 
+def main(tif_filename):
+    # input data
+    tfw_filename = tif_filename.split(".")[0] + r".tfw"
+
+    # read data
+    elevation_data = utils.get_elevation_data_from_tif(tif_filename)
+    tfw = utils.get_coordinates_from_tfw(tfw_filename)
+
+    # Flow analysis
+    dem = rd.LoadGDAL(tif_filename, no_data=-9999)
+
+    #Fill depressions with epsilon gradient to ensure drainage
+    fd_epsilon = rd.FillDepressions(dem, epsilon=True, in_place=False)
+
+    #Get flow accumulation with no explicit weighting. The default will be 1.
+    accum_d8 = rd.FlowAccumulation(fd_epsilon, method='D8')
+
+    # fill depression analysis
+    rda_fill = rd.FillDepressions(dem, epsilon=False, in_place=False)
+    rda_fill_diff = rda_fill - dem
+    rd.rdShow(rda_fill_diff, zxmin=750, zxmax=850, zymin=750, zymax=550, figsize=(8,5.5), cmap='jet')
+
+    # save geojson
+    export_sample = True
+    if export_sample:
+        n_x = min(200, elevation_data.shape[0])
+        n_y = min(200, elevation_data.shape[1])
+    else:
+        n_x = elevation_data.shape[0]
+        n_y = elevation_data.shape[1]
+    
+    geojson_data = utils.get_coordinate_center_points_from_tfw(tfw, elevation_data, accum_d8, rda_fill_diff, n_x, n_y)
+    utils.save_to_geojson(geojson_data, "64_3_2023")
+
+if __name__ == "__main__":
+    main(r"/home/chris/projects/ifk-storm-water-management/data/64_3_2023.tif")
--- src/storm_water_management/utils.py
+++ src/storm_water_management/utils.py
@@ -1,13 +1,13 @@
-import geojson
 import numpy as np
 import rasterio
 import richdem
-
+import json
 
 def get_coordinate_center_points_from_tfw(
     tfw: list,
-    elev_data: np.array,
+    elev_data: richdem.rdarray,
     flow_data: richdem.rdarray,
+    depression_filling_data: richdem.rdarray,
     n_points_x: int,
     n_points_y: int,
 ) -> list:
@@ -17,7 +17,7 @@ def get_coordinate_center_points_from_tfw(
         tfw:
 
     Retrun:
-        elevation data
+        elevation data, flow data, and depression fill
     """
     size_x = tfw[0]
     size_y = tfw[3]
@@ -30,13 +30,15 @@ def get_coordinate_center_points_from_tfw(
             center_x = upper_left_x + (col * size_x)
             center_y = upper_left_y - (row * size_y)
             points.append(
-                geojson.Feature(
-                    geometry=geojson.Point((center_x, center_y)),
-                    properties={
+                {
+                    "type": "Feature", 
+                    "geometry": {"type": "Point", "coordinates": [center_x, center_y]},
+                    "properties":{
                         "elevation_data": float(elev_data[row][col]),
-                        "water_flow": flow_data[row][col],
-                    },
-                )
+                        "water_flow": float(flow_data[row][col]),
+                        "depression_filling": float(depression_filling_data[row][col])
+                        },
+                    }
             )
 
     return points
@@ -56,7 +58,6 @@ def get_elevation_data_from_tif(filename: str) -> np.array:
 
     return elevation_data[0]
 
-
 def get_coordinates_from_tfw(filename: str) -> np.array:
     """Get elevation data
 
@@ -67,27 +68,20 @@ def get_coordinates_from_tfw(filename: str) -> np.array:
         elevation data
     """
     with open(filename, "r") as f:
+
         tfw = [float(x) for x in f.readlines()]
 
     return tfw
 
+def save_to_geojson(data: list, filname: str):
+    """Get elevation data
 
-if __name__ == "__main__":
-    import numpy as np
+    Args:
+        data: data to save
+        filename: name of file to save
 
-    tif_filename = r"/home/chris/projects/ifk-storm-water-management/data/64_3_2023.tif"
-    tfw_filename = tif_filename.split(".")[0] + r".tfw"
-
-    elevation_data = get_elevation_data_from_tif(tif_filename)
-    tfw = get_coordinates_from_tfw(tfw_filename)
-
-    elev_max = np.amax(elevation_data)
-    # 102.80466 , 103.67129 , 105.40455 , 106.492455, 106.93501
-    print(type(float(elevation_data[0][0])))
-    list(elevation_data[0][0:5])
-
-    coordinates = get_coordinate_center_points_from_tfw(tfw, elevation_data, 20, 20)
-    print(coordinates[0])
-
-    with open("temp_file.json", "w") as f:
-        geojson.dump(coordinates, f)
+    Retrun:
+        elevation data
+    """
+    with open(f"{filname}.json", "w") as f:
+        json.dump(data, f)
