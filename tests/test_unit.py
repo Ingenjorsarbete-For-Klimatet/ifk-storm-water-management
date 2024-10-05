cca5bdc7ecb40d5e0827a2a9f36f8496e734d20d
Christoffer Strömberg <chris.stromberg@icloud.com>
2024-10-05 20:46:34 +0200

    test removed

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,8 +1,5 @@
 """Temporary unit test."""
 
-from storm_water_management.storm_water_management import temp
-
-
 def test_unit_temp():
     """Unit test of temp function."""
-    assert temp() is True
+    pass
