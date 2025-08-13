0168b284bd42dcc9c927fd2e7f193b1964956c37
chris <chris.stromberg@icloud.com>
2025-08-13 09:10:12 +0200

    change to whitebox

--- src/storm_water_management/main.py
+++ src/storm_water_management/main.py
@@ -1,1 +1,46 @@
+filename_path = '/home/chris/repos/storm_temp/data/1m'
+filename = '63950_3150_25.tif'
+
+from whitebox_workflows import WbEnvironment, show
+import matplotlib.pyplot as plt
+
+from utils import info, get_tif_as_np_array, get_tif_from_np_array
+
+def main():
+
+    # create environment
+    wbe = WbEnvironment()
+    wbe.verbose = True
+    wbe.working_directory = filename_path
+    dem = wbe.read_raster(filename)
+
+    raster_as_array = get_tif_as_np_array(filename_path, filename)
+    dem_from_array = get_tif_from_np_array(dem, raster_as_array)
+    info(dem_from_array)
+
+    # Smooth DEM. Parameters need to be set to proper values. 
+    #dem_smoothed = wbe.feature_preserving_smoothing(dem_from_array, filter_size=11, normal_diff_threshold=10.0, iterations=3)
+    dem_smoothed = dem_from_array
+    # Fill depressions
+    dem_no_deps = wbe.fill_depressions_planchon_and_darboux(dem_smoothed, flat_increment=0.001)
+    #dem_no_deps = wbe.fill_depressions(dem_smoothed, flat_increment=0.001)
+    #dem_no_deps = wbe.fill_depressions_wang_and_liu(dem_smoothed, flat_increment=0.001)
+    depression_depth = wbe.raster_calculator("('dem_no_deps'-'dem')",[dem_no_deps, dem_from_array])
+    #wbe.write_raster(depression_depth, filename + 'depression_depth.tif')
+
+    # Flow accumulation analysis
+    #channel_threshold = 50000.0
+    #flow_accum = wbe.qin_flow_accumulation(dem_no_deps, out_type='cells', convergence_threshold=channel_threshold, log_transform=True)
+
+    #wbe.write_raster(flow_accum, filename + 'flow_accum.tif'))
+
+    # Plot depression filling
+    fig, ax = plt.subplots()
+    ax = show(depression_depth, ax=ax, title='Depression Filling', figsize=(10,7), colorbar_kwargs={'label': 'Elevation (m)', 'location': "right", 'shrink': 0.5}, zorder=1, vmin=0, vmax=1)
+    ax.legend() 
+    plt.show()
+
+    return depression_depth
+
+main()
 
--- src/storm_water_management/utils.py
+++ src/storm_water_management/utils.py
@@ -1,64 +1,13 @@
-import geojson
+"""Utils."""
+
+import json
+from PIL import Image
 import numpy as np
-import rasterio
-import richdem
+from whitebox_workflows import WbEnvironment
+from pyproj import Transformer
 
-
-def get_coordinate_center_points_from_tfw(
-    tfw: list,
-    elev_data: np.array,
-    flow_data: richdem.rdarray,
-    n_points_x: int,
-    n_points_y: int,
-) -> list:
-    """Get coordinates for centerpoints
-
-    Args:
-        tfw:
-
-    Retrun:
-        elevation data
-    """
-    size_x = tfw[0]
-    size_y = tfw[3]
-    upper_left_x = tfw[4] + size_x / 2
-    upper_left_y = tfw[5] - size_y / 2
-
-    points = []
-    for row in range(n_points_y):
-        for col in range(n_points_x):
-            center_x = upper_left_x + (col * size_x)
-            center_y = upper_left_y - (row * size_y)
-            points.append(
-                geojson.Feature(
-                    geometry=geojson.Point((center_x, center_y)),
-                    properties={
-                        "elevation_data": float(elev_data[row][col]),
-                        "water_flow": flow_data[row][col],
-                    },
-                )
-            )
-
-    return points
-
-
-def get_elevation_data_from_tif(filename: str) -> np.array:
-    """Get elevation data
-
-    Args:
-        filename: name of tif file
-
-    Retrun:
-        elevation data
-    """
-    with rasterio.open(filename) as f:
-        elevation_data = f.read()
-
-    return elevation_data[0]
-
-
-def get_coordinates_from_tfw(filename: str) -> np.array:
-    """Get elevation data
+def get_coordinates_from_tfw(filename: str) -> list:
+    """Get coordinates from tfw.
 
     Args:
         filename: name of tif file
@@ -72,22 +21,124 @@ def get_coordinates_from_tfw(filename: str) -> np.array:
     return tfw
 
 
-if __name__ == "__main__":
+def save_to_geojson(data: list, filename: str) -> None:
+    """Save to geojson.
+
+    Args:
+        data: data to save
+        filename: name of file to save
+    """
+    with open(f"{filename}.json", "w") as f:
+        json.dump(data, f)
+
+def transform_geojson_points_to_polygons(filename: str, output_filename: str = "kvadrater.geojson") -> None:
+    """Transform points to polyhon
+
+    Args:
+        filename: name of geojson file
+        output_filename: name of output file
+    Retrun:
+        None
+    """
+    import geopandas as gpd
+    from shapely.geometry import box
     import numpy as np
 
-    tif_filename = r"/home/chris/projects/ifk-storm-water-management/data/64_3_2023.tif"
-    tfw_filename = tif_filename.split(".")[0] + r".tfw"
+    gdf = gpd.read_file(filename)
 
-    elevation_data = get_elevation_data_from_tif(tif_filename)
-    tfw = get_coordinates_from_tfw(tfw_filename)
+    # Robust beräkning av cellstorlek
+    x_coords = sorted([p.x for p in gdf.geometry])
+    x_diffs = np.diff(x_coords)
+    cell_size = np.median(x_diffs[x_diffs > 0])  # eller sätt värde manuellt
 
-    elev_max = np.amax(elevation_data)
-    # 102.80466 , 103.67129 , 105.40455 , 106.492455, 106.93501
-    print(type(float(elevation_data[0][0])))
-    list(elevation_data[0][0:5])
+    print(f"Beräknad cellstorlek: {cell_size}")
 
-    coordinates = get_coordinate_center_points_from_tfw(tfw, elevation_data, 20, 20)
-    print(coordinates[0])
+    def create_square_around_point(point, size):
+        half = size / 2
+        return box(point.x - half, point.y - half, point.x + half, point.y + half)
 
-    with open("temp_file.json", "w") as f:
-        geojson.dump(coordinates, f)
+    gdf["geometry"] = gdf.geometry.apply(lambda p: create_square_around_point(p, cell_size))
+
+    gdf.to_file(output_filename, driver="GeoJSON")
+
+
+def info(dem) -> None:
+    """Print meta data of tif file
+
+    Args:
+        dem: tif file
+    Retrun:
+        None
+    """
+
+    print(f'Rows: {dem.configs.rows}')
+    print(f'Columns: {dem.configs.columns}')
+    print(f'Resolution (x direction): {dem.configs.resolution_x}')
+    print(f'Resolution (y direction): {dem.configs.resolution_y}')
+    print(f'North: {dem.configs.north}')
+    print(f'South: {dem.configs.south}')
+    print(f'East: {dem.configs.east}')
+    print(f'West: {dem.configs.west}')
+    print(f'Min value: {dem.configs.minimum}')
+    print(f'Max value: {dem.configs.maximum}')
+    print(f'EPSG code: {dem.configs.epsg_code}') # 0 if not set
+    print(f'Nodata value: {dem.configs.nodata}')
+    print(f'Data type: {dem.configs.data_type}') 
+    print(f'Photometric interpretation: {dem.configs.photometric_interp}')
+#transform_geojson_points_to_polygons("/home/chris/repos/ifk-storm-water-management/notebooks/FillDepressionsBestCoast.geojson")
+
+def get_tif_as_np_array(filename_path: str, filename: str) -> np.array:
+    """Transform tif raster to numpy array.
+
+    Args:
+        filename_path: name of directory including file
+        filename: name of tif file
+
+    Retrun:
+        raster as numpy array
+    """
+    im = Image.open(filename_path + "/" + filename)
+    return np.array(im)
+
+def get_tif_from_np_array(dem, tif_as_array: np.array):
+    """Get tif raster from numpy array
+
+    Args:
+        dem: raster
+        tif_as_array: name of tif file
+
+    Retrun:
+        raster
+    """
+    out_configs = dem.configs
+
+    # verkar vara EPSG:5845 i.e.,  Horizontal CRS: EPSG:3006 Vertical CRS: EPSG:5613 
+    # a little bit unclear if 3006 or 5845
+    transformer = Transformer.from_crs("EPSG:5845", "EPSG:4326")
+    lower_lat, lower_lon = transformer.transform(dem.configs.west, dem.configs.south)
+    upper_lat, upper_lon = transformer.transform(dem.configs.east, dem.configs.north)
+
+    out_configs.east = upper_lat
+    out_configs.north = upper_lon
+    out_configs.west = lower_lat
+    out_configs.south = lower_lon
+    out_configs.epsg_code = 4326
+    out_configs.resolution_x = (upper_lat - lower_lat) / (dem.configs.east - dem.configs.west)
+    out_configs.resolution_y = (upper_lon - lower_lon) / (dem.configs.north - dem.configs.south)
+    # min/max will be set when dem saved?
+    #out_configs.minimum = -10
+    #out_configs.maximum = 200
+
+    wbe = WbEnvironment()
+    #wbe.verbose = True
+    #wbe.working_directory = filename_path
+    topo = wbe.new_raster(out_configs)
+
+    num_rows, num_cols = tif_as_array.shape
+    
+    for row in range(num_rows):
+        for col in range(num_cols):
+            topo[row, col] = float(tif_as_array[row, col])
+            #topo[row, col] = float(tif_as_array[row, col])
+
+    return topo
